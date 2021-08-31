cdc0f14a5499d17fe36d017f491459e9e4eff2b0
= <=>
2021-08-31 19:14:02 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,47 @@
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+});
+/*<script type="text/javascript" src="./main.js"></script>
+ */
+
+/*<ul class="slider">
+       <li class="list"><img src="img/acordo.png"></li>
+       <li class="list"><img src="img/credit-card.png"/></li>
+       <li class="list"><img src="img/empresa.png"/></li>
+   </ul>
+   .list{
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    position: absolute;
+}
+.slider{
+    height: 400px;
+    width: 1280px;
+    margin: auto;
+    margin-right: 100px;
+    margin-left: -50px;
+}
+.slider img{
+    margin: auto;
+    height: 100%;
+    width: 107%;
+}
+
+        <div class="col-100 footer">
+            <div class="content">
+                <div class="first">
+                    <ul>
+                        <li><a>Pessoa Física</a></li>
+                        <li><a>Pessoa Jurídica</a></li>
+                        <li><a>Setor Público</a></li>
+                        <li><a>Outras atuações</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+   */
